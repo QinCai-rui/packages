126ce9d09f421571ce9f7b24289ee4dd5ef78acc
Raymont Qin <140027854+QinCai-rui@users.noreply.github.com>
2025-07-11 15:47:50 +1200

    bug fix

--- build-mdllama-rpm.sh
+++ build-mdllama-rpm.sh
@@ -55,6 +55,5 @@ fi
 # 8. Clean up
 rm -rf mdllama
 sudo pip uninstall mdllama -y
-sudo dnf reinstall python3-mdllama -y
 
 echo "Done! The RPM package and repo metadata are in rpm-out/. You can now distribute or upload it to a Fedora repo."
